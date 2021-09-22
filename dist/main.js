1a5bbcb6176025b480d3a7afca5d9a18b0231e1d
Karel <karelkarunia24@gmail.com>
2021-09-22 18:39:37 +0200

    fixing three import

--- dist/main.js
+++ dist/main.js
@@ -1,4 +1,4 @@
-import * as THREE from '../node_modules/three/build/three.module.js';
+import * as THREE from 'https://unpkg.com/three@0.127.0/build/three.module.js';
 
 const scene = new THREE.Scene();
 
